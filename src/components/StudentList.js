b72d9b18acb978a925c264a623976454bf4f1617
parthsharma1410 <parth.sharma2019a@vitstudent.ac.in>
2022-09-01 09:37:14 +0530

    updated studentList

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -109,7 +109,7 @@ export default function StudentList() {
                <input type="number" class="form-control" id="id" aria-describedby="id" onChange={({ target }) => setId(target.value)}/>
                 </div>
          <div class="mb-3">
-              <label for="name" class="form-label">College Name</label>
+              <label for="name" class="form-label">Student Name</label>
                <input type="string" class="form-control" id="name" aria-describedby="name" onChange={({ target }) => setName(target.value)}/>
                 </div>
                 <div class="mb-3">
